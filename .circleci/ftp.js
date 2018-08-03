2988393621f1e9f5858f985997a91823dcab6154
Presse Matthieu <matthieu.presse@ouest-france.fr>
2018-08-03 14:30:57 +0200

    [TECH] circleci

---
+++ .circleci/ftp.js
@@ -0,0 +1,21 @@
+var FtpDeploy = require('ftp-deploy');
+var ftpDeploy = new FtpDeploy();
+
+var config = {
+    username: process.env.FTP_USER,
+    password: process.env.FTP_PWD,
+    host: process.env.FTP_HOST,
+    port: 21,
+    localRoot: __dirname + "/../dist/",
+    remoteRoot: "/",
+    include: [
+        'index.html',
+        'js/sipa-cmp.min.js',
+        'js/sipa-cmp.stub.min.js',
+    ]
+}
+
+ftpDeploy.deploy(config, function(err) {
+    if (err) console.log(err)
+    else console.log('finished');
+});
--- .circleci/use-local-config.sh
+++ .circleci/use-local-config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 curl --user ${CIRCLE_TOKEN}: \
     --request POST \
-    --form revision=8639a138b31a89ad1ac6b82f5cad1b57fe5389e8 \
+    --form revision=HEAD \
     --form config=@config.yml \
     --form notify=false \
         https://circleci.com/api/v1.1/project/github/Ouest-France/cmp
