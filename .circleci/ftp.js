e1e21a2a3dd6329a05e30497a68f7a3d91b9e5bf
Presse Matthieu <matthieu.presse@ouest-france.fr>
2018-08-03 14:56:48 +0200

    [TECH] cirecleci

--- .circleci/ftp.js
+++ .circleci/ftp.js
@@ -1,7 +1,7 @@
-var FtpDeploy = require('ftp-deploy');
-var ftpDeploy = new FtpDeploy();
+var ftp = require('ftp-deploy');
+var ftp = new ftp();
 
-var config = {
+ftp.deploy({
     user: process.env.FTP_USER,
     password: process.env.FTP_PWD,
     host: process.env.FTP_HOST,
@@ -9,13 +9,11 @@ var config = {
     localRoot: __dirname + "/../dist/",
     remoteRoot: "/sipaof/sipacmp",
     include: [
-        'index.html',
-        'js/sipa-cmp.min.js',
-        'js/sipa-cmp.stub.min.js',
-    ]
-}
-console.log(config)
-ftpDeploy.deploy(config, function(err) {
+        '*.html',
+        'js/*.js'
+    ],
+    exclude: []
+}, function(err) {
     if (err) console.log(err)
     else console.log('finished');
 });
