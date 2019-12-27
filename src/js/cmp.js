ebeee73ec717252242f616661de536823e233544
Presse Matthieu <matthieu.presse@ouest-france.fr>
2019-12-27 15:03:05 +0100

    fix: détection localstorage plus poussée

--- src/js/cmp.js
+++ src/js/cmp.js
@@ -1,5 +1,30 @@
 ;(function(cn){
-    if(!window.localStorage) return;
+    // https://developer.mozilla.org/fr/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API#D%C3%A9tection_de_la_fonction_localStorage
+    function storageAvailable(type) {
+        try {
+            var storage = window[type],
+                x = '__storage_test__';
+            storage.setItem(x, x);
+            storage.removeItem(x);
+            return true;
+        }
+        catch(e) {
+            return e instanceof DOMException && (
+                // everything except Firefox
+                e.code === 22 ||
+                // Firefox
+                e.code === 1014 ||
+                // test name field too, because code might not be present
+                // everything except Firefox
+                e.name === 'QuotaExceededError' ||
+                // Firefox
+                e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+                // acknowledge QuotaExceededError only if there's something already stored
+                storage.length !== 0;
+        }
+    }
+
+    if(!storageAvailable('localStorage')) return;
     var __cmp_version__ = 4;
 
     // polyfill
